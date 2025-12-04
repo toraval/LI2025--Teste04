ad85bbb44f7f968811448c509d9a7722d81aa057
antonio valente <antoniorpvalente@gmail.com>
2025-12-04 14:56:22 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 //ficheiro de script
+
+(() => {
+  'use strict';
+
+  const form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+  }, false);
+})();
